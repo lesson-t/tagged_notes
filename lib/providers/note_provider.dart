510dfa575ac2a29eb8986dc9c71a5f1036d63651
takeshi <goozooloo123@gmail.com>
2025-11-29 21:49:26 +0900

    provider togglePin 見つからない場合を改善

--- lib/providers/note_provider.dart
+++ lib/providers/note_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tagged_notes/models/note.dart';
 
 class NoteProvider with ChangeNotifier {
-  // 内部で持っている Todo の一覧
+  // 内部で持っている Note の一覧
   final List<Note> _notes = [];
 
 
@@ -33,7 +33,10 @@ class NoteProvider with ChangeNotifier {
 
   // ピン切り替え
   void togglePin(int id) {
-    final note = _notes.firstWhere((n) => n.id == id);
+    final note = _notes.firstWhere(
+      (n) => n.id == id,
+      orElse: () => throw Exception('Note not found: id=$id')
+    );
     note.togglePin();
     notifyListeners();
   }
