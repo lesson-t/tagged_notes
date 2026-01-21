02b19f43c01e90f13f20e46a9a17b6cc77378d92
takeshi <goozooloo123@gmail.com>
2026-01-21 22:06:55 +0900

    dart format 実行

--- lib/presentation/note_notifier.dart
+++ lib/presentation/note_notifier.dart
@@ -76,14 +76,13 @@ class NoteListNotifier extends AsyncNotifier<NoteListState> {
     await _runMutation(() async {
       final update = ref.read(updateNoteUsecaseProvider);
 
-
       final notes = await update.execute(
-        id: id, 
-        title: title, 
-        body: body, 
-        tag: tag
+        id: id,
+        title: title,
+        body: body,
+        tag: tag,
       );
-      
+
       return notes;
     });
   }
